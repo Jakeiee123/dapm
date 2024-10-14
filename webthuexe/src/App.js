dda1e83a00499aa1f3d81e7b83e252f599f817f8
Ha Duc <hathienphu218@gmail.com>
2024-10-14 16:02:16 +0700

    update layout

--- webthuexe/src/App.js
+++ webthuexe/src/App.js
@@ -1,18 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Duc Ha 
-        </p>
-       
-      </header>
-    </div>
-  );
+  return <Layout />;
 }
 
 export default App;
